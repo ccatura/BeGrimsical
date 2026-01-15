39d0480f17a686f58cc2dce478fe18064ad2a07d
ccatura <charlie@charliecatura.com>
2026-01-15 15:58:01 -0500

    added foreach for links and popups

--- scripts.js
+++ scripts.js
@@ -1,11 +1,26 @@
-var socialLink = document.getElementById("social");
-var socialLinksPopUp = document.getElementById("social-links");
+var socialLinksParents  = document.querySelectorAll(".popup-trans-bg");
+var socialLinksChilden  = document.querySelectorAll(".floating-link-box");
+var topLinks            = document.querySelectorAll(".link-box");
 
 
-socialLink.addEventListener("click", function() {
-    if (socialLinksPopUp.style.display === "flex") {
-        socialLinksPopUp.style.display = "none";
-    } else {
-        socialLinksPopUp.style.display = "flex";
-    }
+socialLinksChilden.forEach((child) => {
+    child.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
 });
+
+// Add event listeners to each top link to show corresponding popup
+topLinks.forEach((link) => {
+    console.log(link.dataset.id);
+    link.addEventListener("click", function() {
+        document.getElementById(link.dataset.id).style.display = "flex"; // show the popup of the clicked link with matching id
+    });
+});
+
+// Add event listeners to each popup background to hide on click
+socialLinksParents.forEach((parent) => {
+    parent.addEventListener("click", function() {
+        parent.style.display = "none";
+    });
+});
+
