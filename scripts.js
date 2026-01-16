e9876d2419e4e2bdbd61705b3def60dcb1204002
ccatura <charlie@charliecatura.com>
2026-01-16 10:56:43 -0500

    fixed email script

--- scripts.js
+++ scripts.js
@@ -2,6 +2,29 @@ var socialLinksParents  = document.querySelectorAll(".popup-trans-bg");
 var socialLinksChilden  = document.querySelectorAll(".floating-link-box");
 var topLinks            = document.querySelectorAll(".link-box");
 
+// Email elements
+var goodToGo            = document.getElementById('good-to-go');
+var goodToGoInput       = document.getElementsByName('good-to-go');
+var sendEmailButton     = document.getElementById('send-email');
+
+goodToGo.addEventListener('click', function() {
+    if (goodToGo.checked == false) {
+        goodToGoInput[0].value = true;
+        sendEmailButton.disabled = false;
+    } else {
+        goodToGoInput[0].value = false;
+        sendEmailButton.disabled = true;
+    }
+});
+
+// Clears the answers to the form once the submit button is clicked
+// clickEmailModal.addEventListener('click', function() {
+//     showEmailModal();
+//     emailName[0].value = '';
+//     emailEmail[0].value = '';
+//     emailMessage[0].value = '';
+//     goodToGo.checked = true;
+// });
 
 socialLinksChilden.forEach((child) => {
     child.addEventListener('click', (event) => {
