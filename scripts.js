d0f7430ef6b0b9eb676949211ddda879288e6e0c
ccatura <charlie@charliecatura.com>
2026-01-26 14:20:58 -0500

    adding substack iframe

--- scripts.js
+++ scripts.js
@@ -7,23 +7,18 @@ var goodToGo            = document.getElementById('good-to-go');
 var goodToGoInput       = document.getElementsByName('good-to-go');
 var sendEmailButton     = document.getElementById('send-email');
 
+var inputsToClear       = document.querySelectorAll('[data-id="clear-me"]');
 
 
-// var tempSwitch = document.getElementById('temp-switch');
-// tempSwitch.addEventListener('click', function() {
-//     // 1. Get the computed style of the root
-//     const rootStyle = getComputedStyle(document.documentElement);
-//     const currentBorder = rootStyle.getPropertyValue('--frame-borders');
-
-//     if (currentBorder === '0px') {
-//         document.documentElement.style.setProperty('--frame-borders', '1px');
-//     } else {
-//         document.documentElement.style.setProperty('--frame-borders', '0px');
-//     }
-// });
+// After a short delay, this hides the contact popup after clicking send email so the form can be seen being submitted
+sendEmailButton.addEventListener('click', function() {
+    setTimeout (function() {
+        document.getElementById('contact').style.display = "none";
+    }, 2000);
+});
 
 
-
+// Enables or disables the send email button based on the checkbox
 goodToGo.addEventListener('click', function() {
     if (goodToGo.checked == false) {
         goodToGoInput[0].value = true;
@@ -34,17 +29,7 @@ goodToGo.addEventListener('click', function() {
     }
 });
 
-
-// Clears the answers to the form once the submit button is clicked
-// clickEmailModal.addEventListener('click', function() {
-//     showEmailModal();
-//     emailName[0].value = '';
-//     emailEmail[0].value = '';
-//     emailMessage[0].value = '';
-//     goodToGo.checked = true;
-// });
-
-
+// Prevent click events on popup content from propagating to the background
 socialLinksChilden.forEach((child) => {
     child.addEventListener('click', (event) => {
         event.stopPropagation();
@@ -55,14 +40,43 @@ socialLinksChilden.forEach((child) => {
 topLinks.forEach((link) => {
     link.addEventListener("click", function() {
         document.getElementById(link.dataset.id).style.display = "flex"; // show the popup of the clicked link with matching id
+        if (link.dataset.id === "contact") {
+            clearInputs(); // Clear input fields when contact popup is opened
+        };
     });
 });
 
-// Add event listeners to each popup background to hide on click
+// Add event listeners to popup background to hide on click
 socialLinksParents.forEach((parent) => {
     parent.addEventListener("click", function() {
         parent.style.display = "none";
     });
+});
+
+// Function to clear all input fields with the specified data-id
+function clearInputs() {
+    inputsToClear.forEach(element => {
+        element.value = "";
+    });
+}
+
+
+
+
+
+
+
+var tempSwitch = document.getElementById('temp-switch');
+tempSwitch.addEventListener('click', function() {
+    // 1. Get the computed style of the root
+    const rootStyle = getComputedStyle(document.documentElement);
+    const currentBorder = rootStyle.getPropertyValue('--frame-borders');
+
+    if (currentBorder === '0px') {
+        document.documentElement.style.setProperty('--frame-borders', '1px');
+    } else {
+        document.documentElement.style.setProperty('--frame-borders', '0px');
+    }
 });
 
 
