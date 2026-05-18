425dbdf0e0089180107300aee0cf65c5e50f54e4
ccatura <charlie@charliecatura.com>
2026-05-18 19:18:04 -0400

    added etsy

--- products/index.php
+++ products/index.php
@@ -15,7 +15,7 @@
         <div class="body-container">
             <div class="inner-content">
                 <div class="inner-content-title">Grimsical Creations - Popular Products</div>
-                <div class="inner-content-subtitle">Click on a product to go to TeePublic.com</div>
+                <div class="inner-content-subtitle">Click on a product to go to its store online</div>
                 <div class="inner-content-gallery">
                     <a href="https://www.teepublic.com/t-shirt/91403547-atom-charlies-microscopic-universe-branded?store_id=4414046" target="_blank"><img class="preview-panel-image" src="https://images.teepublic.com/derived/production/designs/91403547_0/1777501223/i_m:bi_production_blanks_ipv78ghaupktk9ogkdxg_1462829019,c_132_13_216x,s_630,q_90.jpg" alt=""></a>
                     <a href="https://www.teepublic.com/t-shirt/90229279-ew-gross-people?store_id=4414046" target="_blank"><img class="preview-panel-image" src="https://images.teepublic.com/derived/production/designs/90229279_0/1775605604/i_m:bi_production_blanks_jaeeqnv4r8crsxalh03n_1571669489,c_0_0_470x,s_630,q_90.jpg" alt=""></a>
@@ -25,6 +25,7 @@
                     <a href="https://www.teepublic.com/mug/90228781-atom" target="_blank"><img class="preview-panel-image" src="https://images.teepublic.com/derived/production/designs/90228781_2/1775604473/i_m:bi_production_blanks_w00xdkhjelyrnp8i8wxr_1466696262,c_230_160_x473,bc_0f7b47,s_630,q_90.jpg" alt=""></a>
                     <a href="https://www.teepublic.com/sticker/90229057-sos-your-face" target="_blank"><img class="preview-panel-image" src="https://images.teepublic.com/derived/production/designs/90229057_2/1776093285/i_m:pid_1918,bi_production_blanks_dwvfshnuawffombl173b_1490882115,c_s_auto_br,bc_ffcc00,wmk,s_630,q_90.jpg" alt=""></a>
                     <a href="https://www.teepublic.com/t-shirt/90285362-do-you-even-science?store_id=4414046" target="_blank"><img class="preview-panel-image" src="https://images.teepublic.com/derived/production/designs/90285362_0/1775681856/i_m:bi_production_blanks_mtl53ofohwq5goqjo9ke_1462829015,c_0_0_470x,s_630,q_90.jpg" alt=""></a>
+                    <a href="https://www.etsy.com/listing/4489761363/bookmark-math-6x2-with-attached-string" target="_blank"><img class="preview-panel-image" src="https://i.etsystatic.com/48869809/r/il/0f0185/7968106807/il_794xN.7968106807_e9dn.jpg" alt=""></a>
 
                 </div>
             </div>
