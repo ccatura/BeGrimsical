388c651028a7dbfd235e0058e51c4f8a7132b05b
ccatura <charlie@charliecatura.com>
2026-05-28 20:21:07 -0400

    added video maybe

--- index.php
+++ index.php
@@ -14,6 +14,17 @@
                 <h1 class="inner-content-title">Grimsical Creations: Charlie’s Microscopic Universe</h1>
                 <div class="inner-content-subtitle">Updated: May 12, 2026</div>
                 <div class="inner-content-centered">
+
+                    <video controls poster="/videos/cmu-final.png" width="90%" max-width="800" class="rounded-shadow">
+                        <source src="/videos/cmu-final.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+
+
+
+
+
+
                     <a href="./projects/charlies-meets-delilah"><img class="big-image rounded-shadow" src="./images/book-cover-001.jpg" alt=""></a>
                 </div>
             </div>
