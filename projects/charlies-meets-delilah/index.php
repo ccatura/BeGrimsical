a531e4a96d97d2bbea35f5f16dd6952c1005fbfb
ccatura <charlie@charliecatura.com>
2026-05-29 16:41:01 -0400

    fixed thumbnail fit

--- projects/charlies-meets-delilah/index.php
+++ projects/charlies-meets-delilah/index.php
@@ -17,13 +17,13 @@
                 <div class="inner-content-title">Charlie's Microscopic Universe Panels Preview</div>
                 <div class="inner-content-subtitle">March 16, 2026</div>
                 <div class="inner-content-gallery">
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1xaHycloqtnzLvvFy_o7aARuXXI7GKRhH" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1xaHycloqtnzLvvFy_o7aARuXXI7GKRhH" alt=""></a>
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1rmOtEL7wOeGHz9S_cgTeqXs3YPbt4mOW" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1rmOtEL7wOeGHz9S_cgTeqXs3YPbt4mOW" alt=""></a>
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1zwGRmU1cnh0Cb02IHfkkyFuzCNgeNRi0" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1zwGRmU1cnh0Cb02IHfkkyFuzCNgeNRi0" alt=""></a>
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1SBDdVoAzhUzVDrzSsEGmt3INzYDY61_4" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1SBDdVoAzhUzVDrzSsEGmt3INzYDY61_4" alt=""></a>
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1R_eS2XhoqTDjASZjHXmdjHojcDRdtgn2" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1R_eS2XhoqTDjASZjHXmdjHojcDRdtgn2" alt=""></a>
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1v8QeRxXJ0BJV2Vx7KKWsig_nSiPD2_jn" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1v8QeRxXJ0BJV2Vx7KKWsig_nSiPD2_jn" alt=""></a>
-                    <a href="https://lh3.googleusercontent.com/u/0/d/1OCvN0Z2d11Vn9w2G9rZxxBDip-ZPvGj7" target="_blank"><img class="preview-panel-image rounded-shadow" src="https://lh3.googleusercontent.com/u/0/d/1OCvN0Z2d11Vn9w2G9rZxxBDip-ZPvGj7" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1xaHycloqtnzLvvFy_o7aARuXXI7GKRhH" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1xaHycloqtnzLvvFy_o7aARuXXI7GKRhH" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1rmOtEL7wOeGHz9S_cgTeqXs3YPbt4mOW" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1rmOtEL7wOeGHz9S_cgTeqXs3YPbt4mOW" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1zwGRmU1cnh0Cb02IHfkkyFuzCNgeNRi0" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1zwGRmU1cnh0Cb02IHfkkyFuzCNgeNRi0" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1SBDdVoAzhUzVDrzSsEGmt3INzYDY61_4" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1SBDdVoAzhUzVDrzSsEGmt3INzYDY61_4" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1R_eS2XhoqTDjASZjHXmdjHojcDRdtgn2" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1R_eS2XhoqTDjASZjHXmdjHojcDRdtgn2" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1v8QeRxXJ0BJV2Vx7KKWsig_nSiPD2_jn" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1v8QeRxXJ0BJV2Vx7KKWsig_nSiPD2_jn" alt=""></a>
+                    <a class="preview-panel-image rounded-shadow" href="https://lh3.googleusercontent.com/u/0/d/1OCvN0Z2d11Vn9w2G9rZxxBDip-ZPvGj7" target="_blank"><img src="https://lh3.googleusercontent.com/u/0/d/1OCvN0Z2d11Vn9w2G9rZxxBDip-ZPvGj7" alt=""></a>
 
 
                 </div>
