4274d3701d96eef7d28e548696e8bd9faa6141d6
ccatura <charlie@charliecatura.com>
2026-05-29 11:08:18 -0400

    tweaked video CSS

--- index.php
+++ index.php
@@ -15,17 +15,25 @@
                 <div class="inner-content-subtitle">Updated: May 12, 2026</div>
                 <div class="inner-content-centered">
 
-                    <video controls poster="/videos/cmu-final.png" width="90%" max-width="800" class="rounded-shadow">
+
+
+                <div class="video-box">
+                    <video controls poster="/videos/cmu-final.png" class="rounded-shadow">
                         <source src="/videos/cmu-final.mp4" type="video/mp4">
                         Your browser does not support the video tag.
                     </video>
+                    <div class="video-footer">
+                        <div class="video-icon"><img src="/images/profile.jpg" alt="Video Icon"></div>
+                        <div class="video-title">What is: Charlie’s Microscopic Universe</div>
+                    </div>
+                </div>
 
 
 
 
+<br><br>
 
-
-                    <a href="./projects/charlies-meets-delilah"><img class="big-image rounded-shadow" src="./images/book-cover-001.jpg" alt=""></a>
+                    <!-- <a href="./projects/charlies-meets-delilah"><img class="big-image rounded-shadow" src="./images/book-cover-001.jpg" alt=""></a> -->
                 </div>
             </div>
         </div>
